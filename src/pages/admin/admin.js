78588ec265c18995ebad33e849061cf58a28a96d
Marisa Baltag <marisabaltag28@gmail.com>
2023-06-14 18:56:50 +0300

    admin page- show products

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,1 +1,10 @@
 import "./style.css";
+import { getProducts } from "../../api/getProducts";
+import { createProductRow } from "../../compoments/productRow";
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const products = await getProducts();
+  document.getElementById("prodcutsList").innerHTML = products
+    .map((product) => createProductRow(product))
+    .join("");
+});
